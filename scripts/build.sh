eba5f3792498276c6f2780da9c21f487ceb0340c
xnowbzhao <277876397@qq.com>
2023-06-28 11:07:21 +0800

    Add files via upload

---
+++ scripts/build.sh
@@ -0,0 +1,6 @@
+bash dataset_shapenet/build-fd.sh
+bash dataset_shapenet/build-fn.sh
+bash dataset_shapenet/installcloud.sh
+bash dataset_shapenet/installfd.sh
+bash dataset_shapenet/installfn.sh
+cp -r ../data/list/* ../data/ShapeNet/
